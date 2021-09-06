ebac008f2e0eca2683ad71c86c18b812d8cfbab5
kei-i-wa <kei.kumarg@gmail.com>
2021-09-06 04:00:06 +0000

    ADDスキルチェック掛け算・足し算

---
+++ practice.java
@@ -0,0 +1,27 @@
+// paizaスキルチェック掛け算問題
+import java.util.*;
+public class practice{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1*num2);
+    }
+}
+
+
+
+// paizaスキルチェック足し算問題
+import java.util.*;
+
+
+public class Practice1 {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1+num2);
+    }
+}
