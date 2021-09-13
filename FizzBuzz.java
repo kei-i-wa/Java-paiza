b8615d5a87afd6e8912c5abab27e9f95b5cbafc1
kei-i-wa <kei.kumarg@gmail.com>
2021-09-13 06:30:59 +0000

    ADDFizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for(int i=1; i<=count; i++){
+            if(i%15==0){
+                System.out.println("FizzBuzz");
+            }else if(i%3==0){
+                System.out.println("Fizz");
+            }else if(i%5==0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
