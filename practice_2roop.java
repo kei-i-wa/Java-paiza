dab583cbca119fd70bcb8793f33ec739e07f3030
kei-i-wa <kei.kumarg@gmail.com>
2021-09-22 00:36:33 +0000

    ADD２重ループの練習問題追加

---
+++ practice_2roop.java
@@ -0,0 +1,48 @@
+// https://paiza.jp/works/mondai/double_roop_problems/double_roop_problems__print_column_new
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.print(i );
+        }
+    }
+}
+
+// https://paiza.jp/works/mondai/double_roop_problems/double_roop_problems__print_num_new
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+// https://paiza.jp/works/mondai/double_roop_problems/double_roop_problems__print_column
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.println("1 2 3 4 5");
+        }
+        
+    }
+}
+
