215993d66832ab9a72c5414a85eada99c605de59
kei-i-wa <kei.kumarg@gmail.com>
2021-09-28 14:05:41 +0000

    ADD２重ループ問題

---
+++ practice_roop4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+       
+        for(int i=1; i<=n; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
