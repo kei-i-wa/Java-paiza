334b5711e61ca14babb1a717c6e11ccce838cd38
kei-i-wa <kei.kumarg@gmail.com>
2021-09-29 09:08:09 +0000

    UPDATEループの問題

--- practice_roop4.java
+++ practice_roop4.java
@@ -6,11 +6,14 @@ public class Main {
         // 自分の得意な言語で
         // Let's チャレンジ！！
         Scanner sc = new Scanner(System.in);
+        String kaigyou_code_1 = System.lineSeparator();
         int n = sc.nextInt();
         int k = sc.nextInt();
-       
+              for(int x=1; x<=k; x++){
         for(int i=1; i<=n; i++){
             System.out.print(i+" ");
+        }
+        System.out.print(kaigyou_code_1);
         }
     }
 }
