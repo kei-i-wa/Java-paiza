4e81b4d45b54970db141795318302e3beacacc6c
kei-i-wa <kei.kumarg@gmail.com>
2021-10-06 05:53:04 +0000

    UPDATE練習問題追加

---
+++ practice_array8.java
@@ -0,0 +1,23 @@
+https://paiza.jp/works/mondai/c_rank_level_up_problems/c_rank_std_in_out_step3
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0; i<n; i++){
+            a[i]=sc.nextInt();
+        }
+        for(int x:a){
+         System.out.println(x);
+        }
+        
+    }
+}
+
+
