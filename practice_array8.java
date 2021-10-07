cdbd47ec2f1ff66c3824a7c7c1bdd34cd0412d2b
kei-i-wa <kei.kumarg@gmail.com>
2021-10-07 06:17:00 +0000

    ADD C問題練習

--- practice_array8.java
+++ practice_array8.java
@@ -20,4 +20,18 @@ public class Main {
     }
 }
 
+import java.util.*;
 
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            int a = sc.nextInt() + 1;
+            System.out.println(s + " " + a);
+        }
+    }
+}
