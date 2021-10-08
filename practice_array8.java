e4aa6c17722cda6298b3f04bf6c4aab4fc798900
kei-i-wa <kei.kumarg@gmail.com>
2021-10-08 11:26:39 +0000

    ADD 配列練習

--- practice_array8.java
+++ practice_array8.java
@@ -37,3 +37,21 @@ public class Main {
         }
     }
 }
+
+https://paiza.jp/works/mondai/c_rank_level_up_problems/c_rank_string_step1
+
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            String a = sc.next();
+            System.out.println(a.length());
+        }
+    }
+}
