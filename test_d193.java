e8c27f67b5afbaae13cdb3f3c4882c95c82f44b5
kei-i-wa <kei.kumarg@gmail.com>
2021-10-11 00:41:33 +0000

    ADDスキルチェック

---
+++ test_d193.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = b/a;
+        int d = b%a;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
