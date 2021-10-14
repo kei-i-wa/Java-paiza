93d50a8fbd17dde8038e03179378258656caf38b
kei-i-wa <kei.kumarg@gmail.com>
2021-10-14 11:58:37 +0000

    ADD文字列含むかどうか判定

---
+++ practice_contains.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        if(b.contains(a)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+        
+    }
+}
