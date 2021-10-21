e70286104bf93ff25ac55c0959bd974de0b8c0d4
kei-i-wa <kei.kumarg@gmail.com>
2021-10-21 12:42:56 +0000

    UPDATE大文字小文字判定い

--- practice_equal7.java
+++ practice_equal7.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String s = sc.next();
-
+//
         char c = s.charAt(0);
         if (Character.isUpperCase(c)) {
             System.out.println("YES");
